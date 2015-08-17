844c493c1729da1fe6a65a4becada82ec829f1e1
Brian Muramatsu <btmura@gmail.com>
2015-08-16 18:51:53 -0700

    add date range params, sort data chronologically

--- ponzi.go
+++ ponzi.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
+	"sort"
 	"strconv"
 	"time"
 
@@ -13,7 +15,7 @@ import (
 )
 
 func main() {
-	pd, err := getPriceData("SPY")
+	pd, err := getPriceData("SPY", time.Now().Add(time.Hour*24*5), time.Now())
 	if err != nil {
 		log.Fatalf("getPriceData: %v", err)
 	}
@@ -59,14 +61,45 @@ type price struct {
 	volume int64
 }
 
-func getPriceData(symbol string) ([]price, error) {
-	resp, err := http.Get(fmt.Sprintf("http://www.google.com/finance/historical?q=%s&&output=csv", symbol))
+type priceData []price
+
+func (pd priceData) Len() int {
+	return len(pd)
+}
+
+func (pd priceData) Less(i, j int) bool {
+	return pd[i].date.Before(pd[j].date)
+}
+
+func (pd priceData) Swap(i, j int) {
+	pd[i], pd[j] = pd[j], pd[i]
+}
+
+func getPriceData(symbol string, startDate time.Time, endDate time.Time) (priceData, error) {
+	formatTime := func(date time.Time) string {
+		return date.Format("Jan/02/06")
+	}
+
+	v := url.Values{}
+	v.Set("q", symbol)
+	v.Set("startdate", formatTime(startDate))
+	v.Set("enddate", formatTime(endDate))
+	v.Set("output", "csv")
+
+	u, err := url.Parse("http://www.google.com/finance/historical")
+	if err != nil {
+		return nil, err
+	}
+	u.RawQuery = v.Encode()
+	log.Printf("url: %s", u)
+
+	resp, err := http.Get(u.String())
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
-	var pd []price
+	var pd priceData
 	r := csv.NewReader(resp.Body)
 	for i := 0; ; i++ {
 		record, err := r.Read()
@@ -136,6 +169,8 @@ func getPriceData(symbol string) ([]price, error) {
 			})
 		}
 	}
+
+	sort.Sort(pd)
 
 	return pd, nil
 }
