97edd8d7174961ce9dd37d3f4f3c2aa601ddb326
Brian Muramatsu <btmura@gmail.com>
2015-08-16 17:41:36 -0700

    add event loop

--- ponzi.go
+++ ponzi.go
@@ -10,5 +10,28 @@ func main() {
 	if err := termbox.Init(); err != nil {
 		log.Fatalf("termbox.Init: %v", err)
 	}
+
+	if err := termbox.Clear(termbox.ColorDefault, termbox.ColorDefault); err != nil {
+		log.Fatalf("termbox.Clear: %v", err)
+	}
+
+loop:
+	for {
+		termbox.SetCell(0, 0, 'S', termbox.ColorDefault, termbox.ColorDefault)
+		termbox.SetCell(1, 0, 'P', termbox.ColorDefault, termbox.ColorDefault)
+		termbox.SetCell(2, 0, 'Y', termbox.ColorDefault, termbox.ColorDefault)
+
+		if err := termbox.Flush(); err != nil {
+			log.Fatalf("termbox.Flush: %v", err)
+		}
+
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			if ev.Key == termbox.KeyCtrlC {
+				break loop
+			}
+		}
+	}
+
 	defer termbox.Close()
 }
