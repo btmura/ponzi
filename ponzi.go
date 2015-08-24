da30c34b090e3d940e7026b034d4bd6536d87f09
Brian Muramatsu <btmura@gmail.com>
2015-08-23 18:32:23 -0700

    move trading history to separate file

--- ponzi.go
+++ ponzi.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"net/url"
 	"sort"
 	"strconv"
 	"sync"
@@ -213,143 +209,6 @@ loop:
 			}
 		}
 	}
-}
-
-type tradingHistory []tradingSession
-
-// Len implements sort.Interface.
-func (th tradingHistory) Len() int {
-	return len(th)
-}
-
-// Less implements sort.Interface.
-func (th tradingHistory) Less(i, j int) bool {
-	return th[i].date.Before(th[j].date)
-}
-
-// Swap implements sort.Interface.
-func (th tradingHistory) Swap(i, j int) {
-	th[i], th[j] = th[j], th[i]
-}
-
-type tradingSession struct {
-	date          time.Time
-	open          float64
-	high          float64
-	low           float64
-	close         float64
-	volume        int64
-	change        float64
-	percentChange float64
-}
-
-func getTradingHistory(symbol string, startDate time.Time, endDate time.Time) (tradingHistory, error) {
-	formatTime := func(date time.Time) string {
-		return date.Format("Jan/02/06")
-	}
-
-	v := url.Values{}
-	v.Set("q", symbol)
-	v.Set("startdate", formatTime(startDate))
-	v.Set("enddate", formatTime(endDate))
-	v.Set("output", "csv")
-
-	u, err := url.Parse("http://www.google.com/finance/historical")
-	if err != nil {
-		return nil, err
-	}
-	u.RawQuery = v.Encode()
-	log.Printf("url: %s", u)
-
-	resp, err := http.Get(u.String())
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	var th tradingHistory
-	r := csv.NewReader(resp.Body)
-	for i := 0; ; i++ {
-		record, err := r.Read()
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return nil, err
-		}
-
-		// format: Date, Open, High, Low, Close, Volume
-		if len(record) != 6 {
-			return nil, fmt.Errorf("record length should be 6, got %d", len(record))
-		}
-
-		// skip header row
-		if i != 0 {
-			parseRecordTime := func(i int) (time.Time, error) {
-				return time.Parse("2-Jan-06", record[i])
-			}
-
-			parseRecordFloat := func(i int) (float64, error) {
-				return strconv.ParseFloat(record[i], 64)
-			}
-
-			parseRecordInt := func(i int) (int64, error) {
-				return strconv.ParseInt(record[i], 10, 64)
-			}
-
-			date, err := parseRecordTime(0)
-			if err != nil {
-				return nil, err
-			}
-
-			open, err := parseRecordFloat(1)
-			if err != nil {
-				return nil, err
-			}
-
-			high, err := parseRecordFloat(2)
-			if err != nil {
-				return nil, err
-			}
-
-			low, err := parseRecordFloat(3)
-			if err != nil {
-				return nil, err
-			}
-
-			close, err := parseRecordFloat(4)
-			if err != nil {
-				return nil, err
-			}
-
-			volume, err := parseRecordInt(5)
-			if err != nil {
-				return nil, err
-			}
-
-			th = append(th, tradingSession{
-				date:   date,
-				open:   open,
-				high:   high,
-				low:    low,
-				close:  close,
-				volume: volume,
-			})
-		}
-	}
-
-	// Most recent trading sessions at the front.
-	sort.Reverse(th)
-
-	// Calculate the price change which is today's minus yesterday's close.
-	for i := range th {
-		if i+1 < len(th) {
-			th[i].change = th[i].close - th[i+1].close
-			th[i].percentChange = th[i].change / th[i+1].close * 100.0
-		}
-	}
-
-	return th, nil
 }
 
 type tradingDates []time.Time
