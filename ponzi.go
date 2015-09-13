99821b3b5e1537ee3117f799bfaae8d7714452c1
Brian Muramatsu <btmura@gmail.com>
2015-09-12 17:51:40 -0700

    move time related functions to separate file

--- ponzi.go
+++ ponzi.go
@@ -561,50 +561,6 @@ func saveStockData(sd *stockData) {
 	}()
 }
 
-func isMarketHours() bool {
-	now := time.Now().In(newYorkLoc)
-	if now.Weekday() == time.Saturday || now.Weekday() == time.Sunday {
-		return false
-	}
-
-	opening := time.Date(now.Year(), now.Month(), now.Day(), 9, 30, 0, 0, newYorkLoc)
-	if now.Before(opening) {
-		return false
-	}
-
-	closing := time.Date(now.Year(), now.Month(), now.Day(), 12+4, 0, 0, 0, newYorkLoc)
-	if now.After(closing) {
-		return false
-	}
-
-	return true
-}
-
-func mustLoadLocation(name string) *time.Location {
-	loc, err := time.LoadLocation("America/New_York")
-	if err != nil {
-		panic(fmt.Sprintf("time.LoadLocation: %v", err))
-	}
-	return loc
-}
-
-type sortableTimes []time.Time
-
-// Len implements sort.Interface
-func (st sortableTimes) Len() int {
-	return len(st)
-}
-
-// Less implements sort.Interface
-func (st sortableTimes) Less(i, j int) bool {
-	return st[i].Before(st[j])
-}
-
-// Swap implements sort.Interface
-func (st sortableTimes) Swap(i, j int) {
-	st[i], st[j] = st[j], st[i]
-}
-
 // shortenInt shortens larger numbers and appends a quantity suffix.
 func shortenInt(val int64) string {
 	switch {
