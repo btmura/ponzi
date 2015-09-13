f783b6c7a8af36d303570ad8e2a114624777a21b
Brian Muramatsu <btmura@gmail.com>
2015-09-12 18:33:49 -0700

    use time.Parse in isMarketHours test

--- time_test.go
+++ time_test.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
 
 func TestIsMarketHours(t *testing.T) {
+	parseTime := func(value string) time.Time {
+		t, err := time.ParseInLocation("Mon 1/2/06 3:04 PM", value, newYorkLoc)
+		if err != nil {
+			panic(fmt.Sprintf("time.Parse: %v", err))
+		}
+		return t
+	}
+
 	for _, tt := range []struct {
 		desc string
 		now  time.Time
@@ -13,31 +22,31 @@ func TestIsMarketHours(t *testing.T) {
 	}{
 		{
 			desc: "open between hours",
-			now:  time.Date(2015, time.September, 14, 10, 0, 0, 0, newYorkLoc),
+			now:  parseTime("Mon 9/14/15 10:00 AM"),
 			want: true,
 		},
 		{
 			desc: "too early",
-			now:  time.Date(2015, time.September, 14, 9, 0, 0, 0, newYorkLoc),
+			now:  parseTime("Mon 9/14/15 9:00 AM"),
 		},
 		{
 			desc: "too late",
-			now:  time.Date(2015, time.September, 14, 12+6, 0, 0, 0, newYorkLoc),
+			now:  parseTime("Mon 9/14/15 6:00 PM"),
 		},
 		{
 			desc: "closed on saturday",
-			now:  time.Date(2015, time.September, 12, 10, 0, 0, 0, newYorkLoc),
+			now:  parseTime("Sat 9/12/15 10:00 AM"),
 		},
 		{
 			desc: "closed on sunday",
-			now:  time.Date(2015, time.September, 13, 10, 0, 0, 0, newYorkLoc),
+			now:  parseTime("Sun 9/13/15 10:00 AM"),
 		},
 	} {
 		getNow = func() time.Time {
 			return tt.now
 		}
 		if got := isMarketHours(); got != tt.want {
-			t.Errorf("[%s] isMarketHours() = %t, want %t", tt.desc, got, tt.want)
+			t.Errorf("[%s] %v isMarketHours() = %t, want %t", tt.desc, tt.now, got, tt.want)
 		}
 	}
 }
